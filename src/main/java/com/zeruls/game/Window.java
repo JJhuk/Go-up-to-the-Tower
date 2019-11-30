f264854135f06bd3457946b1fc416871f4d1a919
JJhuk <ly0738j@gmail.com>
2019-12-01 01:20:26 +0900

    setTitle : Go Up The Tower

--- src/main/java/com/zeruls/game/Window.java
+++ src/main/java/com/zeruls/game/Window.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 
 public class Window extends JFrame {
 	public Window() {
-		super.setTitle("Juhyuk is dead");
+		super.setTitle("Go Up The Tower");
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		super.setContentPane(new GamePanel(1024, 768));
 		super.pack();
